2cfc81d94ab3af2a4c8cc7282fc8e1d3a7671bdb
Riyad <nafisrashadul@gmail.com>
2025-04-23 11:20:47 +0800

    changed

--- login.php
+++ login.php
@@ -25,7 +25,7 @@ if ($result->num_rows === 1) {
     $user = $result->fetch_assoc();
 
     if ($password === $user['password']) {
-       header("Location: admin-dashboard.html");
+       header("Location: admin-dashboard1.html");
       exit();
   } else {
       echo "❌ Password did not match";
