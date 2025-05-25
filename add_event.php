9f7771332e9c1f41faf6be953ea39213cff7f309
Riyad <nafisrashadul@gmail.com>
2025-05-25 16:49:05 +0800

    changed

--- add_event.php
+++ add_event.php
@@ -7,7 +7,7 @@ $db = 'school_system';
 $user = 'root';
 $pass = '';
 
-$conn = new mysqli($host, $user, $pass, $db,3306);
+$conn = new mysqli($host, $user, $pass, $db,3307);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
